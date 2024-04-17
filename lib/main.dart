67f5c076c0640578712f2d30560c3bb863518b2b
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-17 01:15:42 -0500

    created 1st page

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Text('Hello world'),
+        body: SafeArea(
+          child: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.black, Colors.blue],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/quiz-logo.png"),
+                  const SizedBox(height: 50.0),
+                  const Text("NBA Quiz App",
+                      style: TextStyle(fontSize: 30.0, color: Colors.white)),
+                  const SizedBox(height: 50.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      print('Hello World!');
+                    },
+                    child: const Text(
+                      'Start Quiz',
+                      style: TextStyle(fontSize: 20.0),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
