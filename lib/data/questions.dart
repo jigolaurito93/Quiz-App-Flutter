e4faa07afea999fcf96134b1f7aea3a4195cd992
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-18 03:46:33 -0500

    quiz question format

---
+++ lib/data/questions.dart
@@ -0,0 +1,33 @@
+import 'package:quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'Which NBA player holds the record for the most career points scored?',
+    ["Kareem Abdul-Jabbar", "Michael Jordan", "LeBron James", "Kobe Bryant"],
+  ),
+  QuizQuestion(
+    'Who is the youngest player to ever score 70 or more points in an NBA game?',
+    ['Kobe Bryant', 'LeBron James', 'Devin Booker', 'Wilt Chamberlains'],
+  ),
+  QuizQuestion(
+    'Which team won the first NBA Championship?',
+    [
+      'Boston Celtics',
+      'Chicago Bulls',
+      'Los Angeles Lakers',
+      'Philadelphia Warriors',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the only player to have won NBA MVP, Coach of the Year, and Executive of the Year awards?',
+    ['Larry Bird', 'Magic Johnson', 'Bill Russell', 'Phil Jackson'],
+  ),
+  QuizQuestion(
+    'Who holds the record for the most triple-doubles in NBA history?',
+    ['LeBron James', 'Oscar Robertson', 'Magic Johnson', 'Russell Westbrook'],
+  ),
+  QuizQuestion(
+    'Which NBA player has won the most NBA Championships as a player?',
+    ['Bill Russell', 'Michael Jordan', 'Kareem Abdul-Jabbar', 'LeBron James'],
+  )
+];
