3c81d9c99fb7b719f09774a1a11cfb0d5a34183e
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-20 03:12:29 -0500

    created restart function and empty the list of answers

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -13,17 +13,53 @@ class QuestionsSummary extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Row(children: [
-              Text(((data['question_index'] as int) + 1).toString()),
+            return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Container(
+                padding: const EdgeInsets.all(9.0),
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromARGB(255, 169, 138, 240),
+                ),
+                child: Text(
+                  ((data['question_index'] as int) + 1).toString(),
+                  style: const TextStyle(
+                      fontSize: 12.0, fontWeight: FontWeight.w600),
+                ),
+              ),
+              const SizedBox(
+                width: 13.0,
+              ),
               Expanded(
                 child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(data['question'] as String),
+                    Text(
+                      data['question'] as String,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                          fontSize: 14.0),
+                    ),
                     const SizedBox(
                       height: 5.0,
                     ),
-                    Text(data['user_answer'] as String),
-                    Text(data['correct_answer'] as String),
+                    Text(
+                      data['user_answer'] as String,
+                      style: const TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 148, 162, 240)),
+                    ),
+                    Text(
+                      data['correct_answer'] as String,
+                      style: const TextStyle(
+                          fontSize: 12.0,
+                          fontWeight: FontWeight.w600,
+                          color: Color.fromARGB(255, 204, 210, 245)),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    )
                   ],
                 ),
               ),
